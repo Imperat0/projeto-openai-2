e7a09e40b7706787fe18ed72f2e8f81299d00f07
Imperat0 <imperato06@gmail.com>
2025-08-10 22:20:52 -0300

    adicionado o helpers

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
+from flask import Flask, render_template, request, Response
+from openai import OpenAI
+from dotenv import load_dotenv
 import os
 from time import sleep
-from flask import Flask, request
-from flask_cors import CORS
-import openai
+from helpers import *
 
-app = Flask(__name__)
-CORS(app)
-app.config['DEBUG'] = True
-app.secret_key = 'alura'
+load_dotenv()
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-cliente = openai.OpenAI()
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-4"
 
-@app.route("/chat", methods=["POST"])
-def chat():
-    prompt = request.json["msg"]
-    resposta = bot(prompt)
-    texto_resposta = resposta.choices[0].message.content
-    return texto_resposta
+app = Flask(__name__)
+app.secret_key = "alura"
+
+contexto = carrega("dados/ecomart.txt")
+
 
 def bot(prompt):
     maximo_tentativas = 1
@@ -27,36 +23,46 @@ def bot(prompt):
     while True:
         try:
             prompt_do_sistema = f"""
-            Você é um chatbot de atendimento a clientes de um e-commerce.
+            Você é um chatbot de atendimento a clientes de um e-commerce. 
             Você não deve responder perguntas que não sejam dados do ecommerce informado!
+            Você deve gerar respostas utilizando o contexto abaixo.
+            
+            # Contexto
+            {contexto}
             """
-
             response = cliente.chat.completions.create(
                 messages=[
-                    {
-                        "role": "system",
-                        "content": prompt_do_sistema
-                    },
-                    {
-                        "role": "user",
-                        "content": prompt
-                    }
+                    {"role": "system", "content": prompt_do_sistema},
+                    {"role": "user", "content": prompt},
                 ],
                 temperature=1,
-                max_tokens=256,
+                max_tokens=300,
                 top_p=1,
                 frequency_penalty=0,
                 presence_penalty=0,
-                model=modelo
+                model=modelo,
             )
             return response
         except Exception as erro:
             repeticao += 1
             if repeticao >= maximo_tentativas:
                 return "Erro no GPT: %s" % erro
-            print('Erro de comunicação com OpenAI: ', erro)
+            print("Erro de comunicação com OpenAI:", erro)
             sleep(1)
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+@app.route("/chat", methods=["POST"])
+def chat():
+    prompt = request.json["msg"]
+    resposta = bot(prompt)
+    texto_resposta = resposta.choices[0].message.content
+    return texto_resposta
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
