66a359d44dc306cd6c35198a132e57e394e4cbd9
Imperat0 <imperato06@gmail.com>
2025-08-12 18:42:35 -0300

    adc no app.py o assistente e thread

--- app.py
+++ app.py
@@ -1,50 +1,69 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from openai import OpenAI
-import os
 from dotenv import load_dotenv
+import os
+from time import sleep
+from helpers import *
+from selecionar_persona import *
 from selecionar_documento import *
+from assistente_ecomart import *
 
 load_dotenv()
-
-app = Flask(__name__)
 
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-4"
 
+app = Flask(__name__)
+app.secret_key = "alura"
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        prompt = request.form["prompt"]
+assistente = criar_assistente()
+thread = criar_thread()
 
-        contexto = selecionar_contexto(prompt)
-        documento_selecionado = selecionar_documento(contexto)
 
-        prompt_sistema = f"""
-        Você é a Luri, um chatbot especializado em tirar dúvidas sobre os produtos do EcoMart (um e-commerce focado em sustentabilidade).
+def bot(prompt):
+    maximo_tentativas = 1
+    repeticao = 0
 
-        # CONTEXTO
-        {documento_selecionado}
+    while True:
+        try:
+            cliente.beta.threads.messages.create(
+                thread_id=thread.id, role="user", content=prompt
+            )
 
-        Siga as seguintes regras:
-        - Tom de voz: Amigável, gentil e divertido.
-        - Personalidade: Engraçada, extrovertida e um pouco "hippie".
-        - Formato: Use emojis e гіfѕ para deixar a conversa mais animada.
-        """
+            run = cliente.beta.threads.runs.create(
+                thread_id=thread.id, assistant_id=assistente.id
+            )
 
-        resposta = cliente.chat.completions.create(
-            model=modelo,
-            messages=[
-                {"role": "system", "content": prompt_sistema},
-                {"role": "user", "content": prompt},
-            ],
-        )
+            while run.status != "completed":
+                run = cliente.beta.threads.runs.retrieve(
+                    thread_id=thread.id, run_id=run.id
+                )
 
-        return render_template(
-            "index.html", resposta=resposta.choices[0].message.content
-        )
+            historico = list(
+                cliente.beta.threads.messages.list(thread_id=thread.id).data
+            )
+            resposta = historico[0]
+            return resposta
 
-    return render_template("index.html", resposta=None)
+        except Exception as erro:
+            repeticao += 1
+            if repeticao >= maximo_tentativas:
+                return "Erro no GPT: %s" % erro
+            print("Erro de comunicação com OpenAI:", erro)
+            sleep(1)
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    prompt = request.json["msg"]
+    resposta = bot(prompt)
+    texto_resposta = resposta.content[0].text.value
+    return texto_resposta
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
